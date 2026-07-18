5f7fddfb0c6065d2d471cd83c331b7095d2cdab6
Almiro de Souza <0almiro0de0souza0@exemplo.com>
2026-07-18 09:10:48 -0300

    first commit

---
+++ src/config/database.cjs
@@ -0,0 +1,13 @@
+module.exports={
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5431,
+    username: 'adminPD',
+    password: '079646535',
+    database: 'dev-EPD-db',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true
+    }
+}
